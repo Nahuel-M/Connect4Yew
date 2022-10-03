a0c20aa60f4dfe32da044adc2a6f2a3377076060
Nahuel-M <na.manterola@gmail.com>
2022-10-03 19:26:27 +0200

    Board component setup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
-use yew::{html, function_component};
+use yew::{html, function_component, Component, Html};
+
+struct Board{
+    board_state : i64,
+}
+
+impl Component for Board{
+    type Message = ();
+    type Properties = ();
+    fn update(&mut self, ctx: &yew::Context<Self>, msg: Self::Message) -> bool {
+        false
+    }
+
+    fn create(ctx: &yew::Context<Self>) -> Self {
+        Self { board_state: 0 }
+    }
+
+    fn view(&self, ctx: &yew::Context<Self>) -> Html {
+        todo!()
+    }
+}
 
 #[function_component(App)]
 fn app() -> Html {
-    html!{<p>{"Test"}</p>}
+    html!{<Board/>}
 }
 
 fn main() {
