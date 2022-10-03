912adfc4d8abbc4f5073844b8f799e14466f31db
Jeroen Weener <jweener1996@gmail.com>
2022-10-03 19:52:23 +0200

    Add print function to Board

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
 use yew::{html, function_component, Component, Html};
 
 struct Board{
-    board_state : i64,
+    board_state_x : i64,
+    board_state_o : i64,
+}
+
+impl Board {
+    fn to_pretty_string(& self) -> String {
+        let mut s = "".to_string();
+        let mut o = self.board_state_o;
+        let mut x = self.board_state_x;
+        for i in 0..42 {
+            let co = o & 1;
+            let cx = x & 1;
+            o >>= 1;
+            x >>= 1;
+            if i % 7 == 0 {
+                s += "\n";
+            }
+            s+= if co == 1 { "O" } else if cx == 1 { "X" } else { "_" };
+        }
+        s
+    }
 }
 
 impl Component for Board{
     type Message = ();
     type Properties = ();
     fn update(&mut self, ctx: &yew::Context<Self>, msg: Self::Message) -> bool {
-        false
+        self.to_pretty_string();
+        true
     }
 
     fn create(ctx: &yew::Context<Self>) -> Self {
-        Self { board_state: 0 }
+        Self { board_state_x: 0b11111110000000, board_state_o: 0b1111111 }
     }
 
     fn view(&self, ctx: &yew::Context<Self>) -> Html {
-        todo!()
+        html!{self.to_pretty_string()}
     }
 }
 
